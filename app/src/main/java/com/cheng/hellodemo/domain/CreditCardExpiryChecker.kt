83f4d767382f4ec9501ee29d72709b6e01e0024a
Chandler Cheng <dc9909@hotmail.com>
2025-05-06 11:42:50 +1200

    Handle invalid string input

--- app/src/main/java/com/cheng/hellodemo/domain/CreditCardExpiryChecker.kt
+++ app/src/main/java/com/cheng/hellodemo/domain/CreditCardExpiryChecker.kt
@@ -1,11 +1,18 @@
 package com.cheng.hellodemo.domain
 
 import java.time.LocalDate
+import java.time.format.DateTimeParseException
 
 object CreditCardExpiryChecker {
 
     fun expiresInThreeYears(inputDate: String): Boolean {
-        val parsedInputDate = LocalDate.parse(inputDate)
+        val parsedInputDate: LocalDate = try {
+            LocalDate.parse(inputDate)
+        } catch (e: DateTimeParseException) {
+            println("invalid input: $inputDate, exception: ${e.message}")
+
+            return true
+        }
         val cutOffDate = LocalDate.now().plusYears(3)
 
         return cutOffDate.isAfter(parsedInputDate)
